036baf5e880a768a0804a6ed53d5f95da1110049
Lawrence Wakefield <lawrence@iamlawrence.me>
2024-01-07 17:15:08 +0000

    Add pass through args for ia

--- capture-stream.sh
+++ capture-stream.sh
@@ -17,12 +17,18 @@ while [[ $# -gt 0 ]]; do
             duration=$((hours * 3600 + minutes * 60 + seconds))
             shift 2
             ;;
+        --ia-args)
+            ia_args="$2"
+            shift 2
+            ;;
         *)
             echo "Unknown option: $1"
             exit 1
             ;;
     esac
 done
+
+IFS=' ' read -ra ia_args <<< "$ia_args"
 
 if [ -z "$target" ] || [ -z "$output" ] || [ -z "$duration" ]; then
     echo "Missing required arguments."
@@ -31,7 +37,7 @@ fi
 
 mkfifo curlout-$output.pipe
 curl $target > curlout-$output.pipe & pid1=$!
-ia upload $output - --remote-name=$output --metadata="title:$output" < curlout-$output.pipe & pid2=$!
+ia upload $output - --remote-name=$output --metadata="title:$output" "${ia_args[@]}" < curlout-$output.pipe & pid2=$!
 
 sleep $duration
 kill $pid1
