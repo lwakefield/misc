24075159b32dd96422eedca99d896625e28d0434
Lawrence Wakefield <lawrence@iamlawrence.me>
2024-01-13 13:44:35 +0000

    Next iteration of stream capturing!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # misc
+
+```
+TZ="Pacific/Auckland" ./capture-stream.sh --target https://streams.95bfm.com/stream112 --name-fmt "95bfm-%s.ogg"
+```
--- capture-stream.sh
+++ capture-stream.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
+cleanup() {
+    echo "Received SIGINT. Cleaning up..."
+    pkill -P "$$"                            # Kill all child processes
+    echo "Cleanup complete. Exiting."
+    exit 1
+}
+trap 'cleanup' SIGINT
+
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --target)
             target="$2"
             shift 2  # Consume both the option and its value
             ;;
-        --output)
-            output="$2"
+        --name-fmt)
+            name_fmt="$2"
             shift 2  # Consume both the option and its value
-            ;;
-        --duration)
-            hours=$(echo "$2" | grep -oE '[0-9]+h' | sed 's/h//')
-            minutes=$(echo "$2" | grep -oE '[0-9]+m' | sed 's/m//')
-            seconds=$(echo "$2" | grep -oE '[0-9]+s' | sed 's/s//')
-            duration=$((hours * 3600 + minutes * 60 + seconds))
-            shift 2
-            ;;
-        --ia-args)
-            ia_args="$2"
-            shift 2
             ;;
         *)
             echo "Unknown option: $1"
@@ -28,19 +25,37 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-IFS=' ' read -ra ia_args <<< "$ia_args"
-
-if [ -z "$target" ] || [ -z "$output" ] || [ -z "$duration" ]; then
+if [ -z "$target" ] || [ -z "$name_fmt" ]; then
     echo "Missing required arguments."
     exit 1
 fi
 
-mkfifo curlout-$output.pipe
-curl $target > curlout-$output.pipe & pid1=$!
-ia upload $output - --remote-name=$output --metadata="title:$output" "${ia_args[@]}" < curlout-$output.pipe & pid2=$!
+archive_n_cleanup () {
+    echo "uploading $1..."
+    ia upload $(basename $1) $1
+    # sleep 15 # fake uploading to test...
+    rm -r $(dirname $1)
+    echo "done uploading + cleaning up $1..."
+}
 
-sleep $duration
-kill $pid1
-tail --pid=$pid1 -f /dev/null
-tail --pid=$pid2 -f /dev/null
-rm curlout-$output.pipe
+# we are streaming in 6h chunks, where chunks always finish on a 6h boundary (eg. 12a 6a 12p 6p)
+while true; do
+    # 00 - open the stream ASAP
+    d=$(mktemp -d)
+    f=$d/$(printf $name_fmt $(date +"%Y-%m-%dT%H-%M-%S"))
+
+    curl -s $target > $f & curl_pid=$!
+
+    # 01 - work out how long to stream for until the next chunk
+    current_time=$(date +%s)
+    next_block=$((current_time - (current_time % (6 * 3600)) + 6 * 3600))         # Calculate when the next 6-hour block starts
+    seconds_until_next_block=$((next_block - current_time))                       # Calculate the number of seconds until the next 6-hour block
+    echo "capturing $f for $(date -u -d "@$seconds_until_next_block" '+%Hh %Mm %Ss')..."
+
+    sleep $seconds_until_next_block
+    kill $curl_pid
+    tail --pid=$curl_pid -f /dev/null
+
+    # 02 - fire n' forget so we can start capturing the next stream
+    archive_n_cleanup $f &
+done
